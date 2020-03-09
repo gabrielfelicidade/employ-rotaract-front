6ddd2cc8c6f6782230a24058339a77c5a3a17945
Gabriel Giovani Felicidade <gabriel.felicidade@etec.sp.gov.br>
2020-03-08 23:01:15 -0300

    Initial structure of project

---
+++ README.md
@@ -0,0 +1,11 @@
+### How to run the project
+
+```
+ng serve
+```
+
+### How to build the project
+
+```
+ng build --prod
+```
---
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { HomepageComponent } from 'app/homepage/homepage.component';
+
+export const AdminLayoutRoutes: Routes = [
+    { path: 'vagas',        component: HomepageComponent },
+];
